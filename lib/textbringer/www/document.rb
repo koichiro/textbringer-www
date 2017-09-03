400cfb3148e788ac0293ca062531835f3def798e
Koichiro Ohba <koichiro@meadowy.org>
2017-09-03 16:18:56 +0900

    Use Oga HTML parser

--- lib/textbringer/www/document.rb
+++ lib/textbringer/www/document.rb
@@ -1,17 +1,49 @@
-require 'nokogiri'
+class Textbringer::Www::SaxHander
 
-class Textbringer::Www::Document < Nokogiri::XML::SAX::Document
   def initialize
-    @content = nil
+    @content = []
+    @link_attr = {}
+
+    @node_on_methods = {
+      "a" => lambda{|name, attrs|
+        @link_attr["href"] = attrs["href"]
+      },
+    }
+
+    @node_after_methods = {
+      "a" => lambda{|name|
+        @content << "[#{@link_attr["text"]}](#{@link_attr["href"]})"
+        @link_attr["href"] = nil
+      },
+      "p" => lambda{|name|
+        @content << "\n"
+      },
+    }
+
   end
 
-  def start_element name, attr=[]
+  def on_element ns, name, attrs={}
+    if @node_on_methods.key?(name)
+      @node_on_methods[name].call(name, attrs)
+    end
   end
 
-  def end_element name
+  def after_element ns, name
+    if @node_after_methods.key?(name)
+      @node_after_methods[name].call(name)
+    end
+  end
+
+  def on_text(text)
+    if @link_attr["href"]
+      @link_attr["text"] = text
+      return
+    end
+    return if text == "\n"
+    @content << text
   end
 
   def text
-    @content.to_s
+    @content.join.to_s
   end
 end
