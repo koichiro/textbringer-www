188cbfd8339f989f16b8bfcbd44c8812de38aac0
Koichiro Ohba <koichiro.ohba@gmail.com>
2017-09-18 14:33:14 +0900

    Fix test environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,13 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "simplecov"
 
 require "textbringer"
-require "textbringer/www"
 
 SimpleCov.profiles.define "textbringer-www" do
   add_filter "/test/"
 end
 SimpleCov.start("textbringer-www")
+
+include Textbringer
+include Commands
 
 require "minitest/autorun"
--- test/textbringer/www_test.rb
+++ test/textbringer/www_test.rb
@@ -1,11 +1,8 @@
 require "test_helper"
+require "textbringer/www"
 
 class Textbringer::WwwTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Textbringer::Www::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
